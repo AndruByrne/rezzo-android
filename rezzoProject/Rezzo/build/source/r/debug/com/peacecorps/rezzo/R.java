59edb902c1d7a41a2fa25867627d4cd68a7d33c5
Ian <ian@humin.com>
2013-06-01 19:17:30 -0700

    ir opts
    s

--- rezzoProject/Rezzo/build/source/r/debug/com/peacecorps/rezzo/R.java
+++ rezzoProject/Rezzo/build/source/r/debug/com/peacecorps/rezzo/R.java
@@ -18,12 +18,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f08000c;
         public static final int button=0x7f080000;
         public static final int checkBox=0x7f080002;
         public static final int checkBox2=0x7f080003;
-        public static final int section_label=0x7f080004;
+        public static final int checkBox3=0x7f080004;
+        public static final int checkBox4=0x7f080005;
+        public static final int checkBox5=0x7f080007;
+        public static final int checkBox6=0x7f080008;
+        public static final int checkBox7=0x7f080009;
+        public static final int checkBox8=0x7f08000a;
+        public static final int section_label=0x7f08000b;
         public static final int textView=0x7f080001;
+        public static final int textView2=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,17 +42,25 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
-        public static final int main_heading1=0x7f050002;
-        public static final int main_heading2=0x7f050003;
-        public static final int main_heading3=0x7f050004;
-        public static final int nr1=0x7f050005;
-        public static final int nr2=0x7f050006;
-        public static final int nr3=0x7f050007;
-        public static final int nr4=0x7f050008;
-        public static final int select_photo_button=0x7f050009;
-        public static final int title_section1=0x7f05000a;
-        public static final int title_section2=0x7f05000b;
-        public static final int title_section3=0x7f05000c;
+        public static final int ir1=0x7f050002;
+        public static final int ir2=0x7f050003;
+        public static final int ir3=0x7f050004;
+        public static final int ir4=0x7f050005;
+        public static final int main_heading1=0x7f050006;
+        public static final int main_heading2=0x7f050007;
+        public static final int main_heading3=0x7f050008;
+        public static final int nr1=0x7f050009;
+        public static final int nr2=0x7f05000a;
+        public static final int nr3=0x7f05000b;
+        public static final int nr4=0x7f05000c;
+        public static final int select_photo_button=0x7f05000d;
+        public static final int sr1=0x7f05000e;
+        public static final int sr2=0x7f05000f;
+        public static final int sr3=0x7f050010;
+        public static final int sr4=0x7f050011;
+        public static final int title_section1=0x7f050012;
+        public static final int title_section2=0x7f050013;
+        public static final int title_section3=0x7f050014;
     }
     public static final class style {
         /** 
